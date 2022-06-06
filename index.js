f37fc6ea8add96528eadf52a09aa178f006a8ee1
Kevin Gicheha <gichehakevin@gmail.com>
2022-06-06 14:26:33 -0700

    commit tree 1e873f7f9b2503d0de6ed37ab4aff7c2d8239cdd

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 
 // Write your code here
+let num1 = 31;
+
+let num2 = 2 ;
+
+let multiply = num1 * num2 ;
+
+
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+
+let  num3 = 22 ;
+
+let num4 = 26;
+
+let mod = num4 % num3;
+
+let max = 20;
+Math.max(max);
